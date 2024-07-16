7d2018206597e0627e78fba5cad6c3caea602d18
Jaeseok Han <33484015+sabill123@users.noreply.github.com>
2024-07-16 17:25:39 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -0,0 +1,11 @@
+def solution(number, n, m):
+    if(number % n == 0 or number % m ==0):
+        if (number % n ==0):
+            if(number % m ==0):
+                return 1
+            else: return 0
+        if (number % m ==0):
+            if(number % n ==0):
+                return 1
+            else: return 0
+    else: return 0
